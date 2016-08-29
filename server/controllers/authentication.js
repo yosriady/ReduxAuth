099fda3bb6ee826aee21cc46275097c2233b18a4
Yos Riady <yosriady@gmail.com>
2016-08-30 00:25:06 +0800

    Complete auth server

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,15 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  res.send({token: tokenForUser(req.user)});
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -23,8 +34,8 @@ exports.signup = function(req, res, next) {
       user.save(function(err){
         if(err) { return next(err); }
 
-        return res.status(201).json({success: true});
-      })
+        return res.status(201).json({token: tokenForUser(user)});
+      });
 
     });
 
