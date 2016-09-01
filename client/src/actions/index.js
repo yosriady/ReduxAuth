af8699c598cf76f17eec9610ec8de051bcbe0916
Yos Riady <yosriady@gmail.com>
2016-09-01 11:30:44 +0800

    Section 7 -102

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,26 @@
 import axios from 'axios';
+import {browserHistory} from 'react-router';
 const ROOT_URL = 'http://localhost:3000';
 
-export function signinUser({email, password}) {
-    console.log(email, password);
-    return function(dispatch){
-        axios.post(`${ROOT_URL}/signin`, { email, password });
+import {AUTH_USER, UNAUTH_USER, AUTH_ERROR} from './types';
 
-        // TODO
+export function signinUser({email, password}) {
+    return function(dispatch){
+        axios.post(`${ROOT_URL}/signin`, { email, password })
+        .then(response => {
+          dispatch({type: AUTH_USER});
+          localStorage.setItem('token', response.data.token);
+          browserHistory.push('/feature');
+        })
+        .catch(() => {
+          dispatch(authError('Bad login'));
+        });
+    }
+}
+
+export function authError(error) {
+    return {
+      type: AUTH_ERROR,
+      error: error
     }
 }
--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app) {
     app.get('/', requireAuth, function(req, res) {
-      res.send('Yo');
+      res.send({message: 'Yo'});
     });
     app.post('/signin', requireSignin, Authentication.signin);
     app.post('/users', Authentication.signup);
